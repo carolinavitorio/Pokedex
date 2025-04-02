c3986dd33bc7142eff23382812ede285b85c9521
carolinavitorio <cah_vitorio@hotmail.com>
2025-04-02 14:23:31 -0300

    Versão com Poke API 1.1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,25 @@
-console.log('Sucesso no carregamento')
+function convertPokemonTypesToLi(pokemonsTypes) {
+    return pokemonsTypes.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`)
+    
+}
+
+function convertPokemonToLi(pokemon) {
+    return `
+        <li class="pokemon">
+            <span class="number">#${pokemon.order}</span>
+            <span class="name">${pokemon.name}</span>
+            <div class="detail">
+                <ol class="types">
+                    ${convertPokemonTypesToLi(pokemon.types).join('')}
+                </ol>
+                <img src="${pokemon.sprites.other.dream_world.front_default}" alt="${pokemon.name}">
+            </div>
+        </li>
+    `
+}
+
+const pokemonList = document.getElementById('pokemonList')
+
+pokeApi.getPokemons().then((pokemons = []) => {
+    pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join ('')
+})
