3a27637172eaea8bcac1f4ffa174035f83d0e587
carolinavitorio <cah_vitorio@hotmail.com>
2025-04-08 18:36:11 -0300

    V2.0 com Modal dinâmica de cada pokemon

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,28 +5,24 @@ const maxRecords = 151
 const limit = 10
 let offset = 0;
 
-function convertPokemonToLi(pokemon) {
-    return `
-        <li class="pokemon ${pokemon.type}">
-            <span class="number">#${pokemon.number}</span>
-            <span class="name">${pokemon.name}</span>
-
-            <div class="detail">
-                <ol class="types">
-                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
-                </ol>
-
-                <img src="${pokemon.photo}"
-                     alt="${pokemon.name}">
-            </div>
-        </li>
-    `
-}
-
 function loadPokemonItens(offset, limit) {
     pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
-        const newHtml = pokemons.map(convertPokemonToLi).join('')
-        pokemonList.innerHTML += newHtml
+        pokemons.forEach(pokemon => {
+            const li = document.createElement('li')
+            li.className = `pokemon ${pokemon.type}`
+            li.innerHTML = `
+          <span class="number">#${pokemon.number}</span>
+          <span class="name">${pokemon.name}</span>
+          <div class="detail">
+            <ol class="types">
+              ${pokemon.types.map(type => `<li class="type ${type}">${type}</li>`).join('')}
+            </ol>
+            <img src="${pokemon.photo}" alt="${pokemon.name}">
+          </div>
+        `
+            li.addEventListener('click', () => abrirModalPokemon(pokemon))
+            pokemonList.appendChild(li)
+        })
     })
 }
 
@@ -45,3 +41,43 @@ loadMoreButton.addEventListener('click', () => {
         loadPokemonItens(offset, limit)
     }
 })
+
+function abrirModalPokemon(pokemon) {
+    const overlay = document.createElement('div');
+    overlay.classList.add('card-container');
+
+    const modalPokemon = document.createElement('div');
+    modalPokemon.classList.add('card-content');
+    modalPokemon.innerHTML = `
+      <div class="topo-card ${pokemon.type}">
+        <button class="card-close" onclick="fecharModal(this)">X</button>
+        <span class="card-number">#${pokemon.number}</span>
+        <span class="card-name">${pokemon.name}</span>
+        <img src="${pokemon.photo}" alt="${pokemon.name}">
+      </div>
+  
+      <div class="detail-card">
+        <span>Abilities</span>
+        <p>${pokemon.abilities.join(', ') || 'Nenhuma'}</p>
+  
+        <span>Held Items</span>
+        <p>${pokemon.heldItems.join(', ') || 'Nenhum'}</p>
+  
+        <span>Moves</span>
+        <p>${pokemon.moves.slice(0, 5).join(', ')}</p>
+  
+        <span>Stats</span>
+        <p>${pokemon.stats.map(stat => `${stat.name}: ${stat.value}`).join(', ')}</p>
+      </div>
+    `
+    overlay.appendChild(modalPokemon);
+    document.body.appendChild(overlay);
+}
+
+
+function fecharModal(btn) {
+    const overlay = btn.closest('.card-container');
+    if (overlay) {
+        document.body.removeChild(overlay);
+    }
+}
--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,37 +1,56 @@
 
 const pokeApi = {}
 
-function convertPokeApiDetailToPokemon(pokeDetail) {
-    const pokemon = new Pokemon()
-    pokemon.number = pokeDetail.id
-    pokemon.name = pokeDetail.name
-
-    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
-    const [type] = types
-
-    pokemon.types = types
-    pokemon.type = type
-
-    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
-
-    pokemon.info = pokeDetail.sprites.other.dream_world.front_default
-
-    return pokemon
+pokeApi.getPokemonDetail = (pokemon) => {
+  return fetch(pokemon.url)
+    .then(response => response.json())
+    .then(data => ({
+      number: data.id,
+      name: data.name,
+      photo: data.sprites.other.dream_world.front_default,
+      type: data.types[0].type.name,
+      types: data.types.map(t => t.type.name),
+      abilities: data.abilities.map(a => a.ability.name),
+      heldItems: data.held_items.map(item => item.item.name),
+      moves: data.moves.map(m => m.move.name),
+      stats: data.stats.map(s => ({
+        name: s.stat.name,
+        value: s.base_stat
+      }))
+    }))
 }
 
-pokeApi.getPokemonDetail = (pokemon) => {
-    return fetch(pokemon.url)
-        .then((response) => response.json())
-        .then(convertPokeApiDetailToPokemon)
+pokeApi.getPokemons = (offset = 0, limit = 10) => {
+  const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+  return fetch(url)
+    .then(response => response.json())
+    .then(data => data.results)
+    .then(pokemonList => Promise.all(pokemonList.map(pokeApi.getPokemonDetail)))
 }
 
 pokeApi.getPokemons = (offset = 0, limit = 5) => {
-    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+  const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
-    return fetch(url)
-        .then((response) => response.json())
-        .then((jsonBody) => jsonBody.results)
-        .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
-        .then((detailRequests) => Promise.all(detailRequests))
-        .then((pokemonsDetails) => pokemonsDetails)
+  return fetch(url)
+    .then((response) => response.json())
+    .then((jsonBody) => jsonBody.results)
+    .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
+    .then((detailRequests) => Promise.all(detailRequests))
+    .then((pokemonsDetails) => pokemonsDetails)
 }
+
+async function fetchPokemonData(pokeId) {
+  const url = `https://pokeapi.co/api/v2/pokemon/${pokeId}`;
+
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error("Erro ao buscar os dados do Pokémon");
+    }
+    const pokemon = await response.json();
+    return pokemon;
+  } catch (error) {
+    console.error("Erro ao buscar os dados do Pokémon:", error);
+    return null;
+  }
+} 
